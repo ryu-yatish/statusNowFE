560007e6907c1eba0c04f2d69b324e941b6fd190
ryu-yatish <yatishagrawal2304@gmail.com>
2024-10-28 10:09:55 +0530

    wording change

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,7 @@ function Home() {
       <h1 className="text-4xl font-bold mb-4">Simple and powerful status page</h1>
       <p className="text-xl mb-8">Monitor your website and API. Get notified when things go wrong.</p>
       <Button asChild className="text-lg px-6 py-3">
-        <Link to="/register"><button className="cta-button">Get Started</button></Link>
+        <Link to="/register"><button className="cta-button">Start Now</button></Link>
       </Button>
       <section className="features mt-16">
         {/* Add feature items here */}
